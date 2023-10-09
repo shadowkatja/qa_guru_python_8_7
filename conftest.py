cd25dc178b722f6f879aab512af4ce0785737aed
Ekaterina Goldinova <shadowkatja@gmail.com>
2023-10-09 10:26:29 +0300

    reformate code

--- conftest.py
+++ conftest.py
@@ -1,9 +1,10 @@
+import os
 import pathlib
+from zipfile import ZipFile, ZIP_DEFLATED
 
 import pytest
-import os
+
 from path import tmp_path, resources_path
-from zipfile import ZipFile, ZIP_DEFLATED
 
 
 @pytest.fixture(scope='function', autouse=True)
@@ -11,6 +12,7 @@ def create_tmp():
     os.mkdir(tmp_path)
     yield
     os.remove(tmp_path)
+
 
 @pytest.fixture(scope='function')
 def create_zip(create_tmp):
