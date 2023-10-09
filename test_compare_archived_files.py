02f0f9b7e32b9d0584e50440437be7d79f9f3dc9
Ekaterina Goldinova <shadowkatja@gmail.com>
2023-10-09 15:36:55 +0300

    add txt check

--- test_compare_archived_files.py
+++ test_compare_archived_files.py
@@ -1,13 +1,21 @@
+import filecmp
 import os
 from zipfile import ZipFile
 
 from path import resources_path, zip_path
 
 
+# check that all files are in archive
 def test_archive():
     resources_files = os.listdir(resources_path)
     with ZipFile(zip_path, 'r') as zf:
-        # check that all files are in archive
         assert resources_files == ZipFile.namelist(zf)
 
-#def test_txt():
+def test_files():
+    with ZipFile(zip_path, 'r') as zf:
+        # check txt
+        with open(os.path.join(resources_path,'Hello.txt')) as txt:
+            original_file = txt.read()
+            assert original_file == zf.read('Hello.txt').decode()
+
+
