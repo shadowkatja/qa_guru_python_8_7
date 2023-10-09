14aef384c325b824752c0126d390d1fdd3dc691a
Ekaterina Goldinova <shadowkatja@gmail.com>
2023-10-09 12:01:21 +0300

    refactor work with tmp dir

--- conftest.py
+++ conftest.py
@@ -4,19 +4,19 @@ from zipfile import ZipFile, ZIP_DEFLATED
 
 import pytest
 
-from path import tmp_path, resources_path
+from path import tmp_path, resources_path, zip_path
 
 
 @pytest.fixture(scope='function', autouse=True)
-def create_tmp():
-    os.mkdir(tmp_path)
-    yield
-    os.remove(tmp_path)
-
-
-@pytest.fixture(scope='function')
-def create_zip(create_tmp):
+def create_zip():
+    if not os.path.exists(tmp_path):
+        os.mkdir(tmp_path)
     folder = pathlib.Path(resources_path)
     with ZipFile(zip_path, 'w', ZIP_DEFLATED) as zf:
         for file in folder.iterdir():
             zf.write(file, arcname=file.name)
+
+    yield
+
+    os.remove(zip_path)
+    os.rmdir(tmp_path)
