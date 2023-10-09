6e45093af237c6a5db8f1c91460e4447bc514092
Ekaterina Goldinova <shadowkatja@gmail.com>
2023-10-09 16:06:45 +0300

    add pdf check

--- test_compare_archived_files.py
+++ test_compare_archived_files.py
@@ -1,9 +1,10 @@
 import filecmp
 import os
 from zipfile import ZipFile
+import random
 
 from path import resources_path, zip_path
-
+from pypdf import PdfReader
 
 # check that all files are in archive
 def test_archive():
@@ -11,11 +12,17 @@ def test_archive():
     with ZipFile(zip_path, 'r') as zf:
         assert resources_files == ZipFile.namelist(zf)
 
-def test_files():
+def test_txt_file():
     with ZipFile(zip_path, 'r') as zf:
-        # check txt
         with open(os.path.join(resources_path,'Hello.txt')) as txt:
             original_file = txt.read()
             assert original_file == zf.read('Hello.txt').decode()
+
+def test_pdf_file():
+    with ZipFile(zip_path, 'r') as zf:
+        original_file = PdfReader(os.path.join(resources_path,'Python Testing with Pytest (Brian Okken).pdf'))
+        archived_file = PdfReader(zf.open('Python Testing with Pytest (Brian Okken).pdf'))
+        assert len(original_file.pages) == len(archived_file.pages)
+        assert original_file.pages[3].extract_text() == archived_file.pages[3].extract_text()
 
 
